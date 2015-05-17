9d1eabb775a54ef64d3ab83e0f10465cad4d981a
mbonczkowska <mbonczkowska@sigma.ug.edu.pl>
2015-05-17 20:23:50 +0200

    Update myscript.js

--- public/myscript.js
+++ public/myscript.js
@@ -8,7 +8,7 @@ $(document).ready(function (){
 		$("#haslo").validate(  {pattern:  /^[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż].*[0-9]|[0-9].*[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]$/ }); 
 	    $("#kodPocztowy").validate( {pattern: /^[0-9]{2}-[0-9]{3}$/} );
 		$.getJSON("kody.json", function(data) {
-			for (i = 0; i < data.length; i++) { 
+			for (var i = 0; i < data.length; i++) { 
 				 if(data[i]["KOD POCZTOWY"] === $("#kodPocztowy").val()){
 					$("#miasto").html(data[i]["MIEJSCOWOŚĆ"]);
 				 }
