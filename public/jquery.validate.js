85be1714d00138338e8666c5f78f8aaba2d1faa6
Magda Bonczkowska <magda1231@gmail.com>
2015-05-17 10:38:55 +0200

    walidacje

---
+++ public/jquery.validate.js
@@ -0,0 +1,21 @@
+(function ($) {
+
+    $.fn.validate = function (options) {
+        return this.each(function () {
+            var entropia = Math.log2(74); // 32 * 2 + 10
+            var entropiaPass = $("#haslo").val().length;
+            $("#entropia").text("Entropia wyniosi:   " + entropia * entropiaPass);
+
+            if (options.pattern.test($(this).val()) === true) {
+                if (entropia * entropiaPass > 50) {
+
+                    $(this).addClass("good");
+                }
+            }
+            else {
+                $(this).addClass("error");
+
+            }
+        });
+    }
+})(jQuery);
