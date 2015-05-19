1667f85a1e6708a5259fa95eb8b6c52f2f6b0f58
Magda Bonczkowska <magda1231@gmail.com>
2015-05-19 19:59:56 +0200

    Poprawki

--- public/jquery.validate.js
+++ public/jquery.validate.js
@@ -1,21 +1,53 @@
-(function ($) {
-
-    $.fn.validate = function (options) {
+﻿(function ($) {
+    $.fn.validatePassword = function (options) {
         return this.each(function () {
+
             var entropia = Math.log2(74); // 32 * 2 + 10
-            var entropiaPass = $("#haslo").val().length;
-            $("#entropia").text("Entropia wyniosi:   " + entropia * entropiaPass);
+            var entropiaPass = $(this).val().length;
+            $(this).next().html("Entropia wyniosi:   " + entropia * entropiaPass);
 
             if (options.pattern.test($(this).val()) === true) {
-                if (entropia * entropiaPass > 50) {
-
-                    $(this).addClass("good");
-                }
+                $(this).addClass("good");
             }
             else {
                 $(this).addClass("error");
 
             }
         });
-    }
+    };
+    $.fn.validateZipCode = function (options) {
+        return this.each(function () {
+
+            var self = $(this);
+
+            $.getJSON("kody.json", function (data) {
+                var pass = false;
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i]["KOD POCZTOWY"] === $(self).val()) {
+                        $(self).next().html(data[i]["MIEJSCOWOŚĆ"]);
+                        pass = true;
+                    }
+                }
+				
+                if (options.pattern.test($(self).val()) === true && pass === true) {
+                    $(self).addClass("good");
+                }
+                else {
+                    $(self).addClass("error");
+                }
+            });
+        });
+    };
+    $.fn.validate = function (options) {
+        return this.each(function () {
+            if (options.pattern.test($(this).val()) === true) {
+                $(this).addClass("good");
+            }
+            else {
+                $(this).addClass("error");
+
+            }
+
+        });
+    };
 })(jQuery);
--- public/myscript.js
+++ public/myscript.js
@@ -1,18 +1,12 @@
 $(document).ready(function (){
 
-	$("#submit").click(function(){
+	$( "form" ).submit(function( event ) {
 		
 		$("#imie").validate( {pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłóńśźż]*$/} );
 		$("#nazwisko").validate( {pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłóńśźż]*$/}  );
 		$("#email").validate(  {pattern:  /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/}); 
-		$("#haslo").validate(  {pattern:  /^[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż].*[0-9]|[0-9].*[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]$/ }); 
-	    $("#kodPocztowy").validate( {pattern: /^[0-9]{2}-[0-9]{3}$/} );
-		$.getJSON("kody.json", function(data) {
-			for (i = 0; i < data.length; i++) { 
-				 if(data[i]["KOD POCZTOWY"] === $("#kodPocztowy").val()){
-					$("#miasto").html(data[i]["MIEJSCOWOŚĆ"]);
-				 }
-			}
-	    });
+		$("#haslo").validatePassword(  {pattern:  /^[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż].*[0-9]|[0-9].*[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]$/ }); 
+	    $("#kodPocztowy").validateZipCode( {pattern: /^[0-9]{2}-[0-9]{3}$/} );
+
     });
 });
