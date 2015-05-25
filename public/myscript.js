75dc092893503ecd16d90dad63e137b1b245c432
mbonczkowska <mbonczkowska@sigma.ug.edu.pl>
2015-05-25 08:56:50 +0200

    Update myscript.js

--- public/myscript.js
+++ public/myscript.js
@@ -5,20 +5,10 @@ $(document).ready(function (){
 		$("#imie").validate( {pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłóńśźż]*$/} );
 		$("#nazwisko").validate( {pattern: /^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłóńśźż]*$/}  );
 		$("#email").validate(  {pattern:  /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/}); 
-<<<<<<< HEAD
+
 		$("#haslo").validatePassword(  {pattern:  /^[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż].*[0-9]|[0-9].*[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]$/ }); 
 	    $("#kodPocztowy").validateZipCode( {pattern: /^[0-9]{2}-[0-9]{3}$/} );
 
-=======
-		$("#haslo").validate(  {pattern:  /^[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż].*[0-9]|[0-9].*[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłóńśźż]$/ }); 
-	    $("#kodPocztowy").validate( {pattern: /^[0-9]{2}-[0-9]{3}$/} );
-		$.getJSON("kody.json", function(data) {
-			for (var i = 0; i < data.length; i++) { 
-				 if(data[i]["KOD POCZTOWY"] === $("#kodPocztowy").val()){
-					$("#miasto").html(data[i]["MIEJSCOWOŚĆ"]);
-				 }
-			}
-	    });
->>>>>>> origin/master
+
     });
 });
